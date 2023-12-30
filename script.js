f8ebdea84b2f1733edfb7221b2ab2acf316b14ab
shaktigc2 <154906783+shaktigc2@users.noreply.github.com>
2023-12-30 16:00:02 +0000

    speed test

---
+++ script.js
@@ -0,0 +1,120 @@
+const paragraphs = [
+    "You are aware that the incumbent government is rapidly working to formulate the federal civil service act, federal education act, police adjustment act and other laws required for the implementation of constitution PM Dahal said adding Bills already presented in the parliament will be brought to a conclusion through discussion. I will put efforts to finalize them through discussion at top level",
+    "At a group meeting with the Chief Ministers of all seven provinces at the Prime Minister's residence, Baluwatar, today, PM Dahal said his focus was on implementing federalism as an architect of federalism and a leader of constitution-making",
+    "Two individuals, who were injured in a clash with police personnel during a demonstration in Balkumari of Lalitpur today, have died. Nepal Police spokesperson, Deputy Inspector General Kuber Kadayat, confirmed the deaths of the two individuals, identified as 22-year-old Birendra Shah from Doti and 23-year-old Sujan Rawat from Dailekh. Both succumbed to their injuries during treatment, he added.Rawat passed away while undergoing treatment at KIST Hospital, while Shah breathed his last at Patan Hospital. The demonstration occurred as youths claimed they were prevented from participating in the EPS examination.",
+    "Minister for Culture, Tourism, and Civil Aviation, Sudan Kirati, has stated that the government has initiated discussions on the possibility of adopting a public-private partnership (PPP) model for the operations of international airports. According to the Minister, these discussions encompass the operations of Tribhuvan International Airport, the recently constructed Gautam Buddha International Airport in Bhairahawa, and the Pokhara International Airport.He shared this information during a meeting of the Delegated Management and Government Assurance Committee under the National Assembly at Singha Durbar on Friday. Additionally, he informed the meeting that efforts are underway to enhance the effectiveness of commercial flights from international airports.",
+    "The Minister expressed concern about the underutilization of newly built international airports and mentioned that 22 domestic airports have already been transformed into pasture lands.He pointed out that despite the extension of Tribhuvan International Airport (TIA) to its full capacity, it will be unable to handle increasing traffic flows in the coming years. Consequently, the Minister argued for the need to expedite the construction of Nijgadh airport.Similarly, lawmakers Nar Bahadur Bista, Gomadevi Timilsina, and Narapati Luhar demanded that the government systematize the Narayanhiti Durbar Museum and objects of archaeological importance.",
+
+    "Finance Minister Dr Prakash Sharan Mahat has said an environment has been created to achieve six per cent economic growth rate, as the country's economy has started returning to its form gradually. Inaugurating the 20th annual general meeting of Bhrikutimandap based Kathmandu Valley Public Library Society today, Minister Mahat mentioned that overall situation of country's economy is satisfactory as arrival of tourists, remittance, share price, revenue and economic activities are increasing.He clarified that anomalies of budget have been controlled by developing budget allocation system based on guarantee of resources in multi-year projects as well as decrease in interest rate of banks has given a positive sign.",
+
+    "The Finance Minister shared that an improvement was made in budget system by ending the trend of extra expenses than stipulated in the budget Red Book.Society president Dr Narayan Khadka underscored on the need of the State to give priority to library as it is the centre of knowledge and information as well as the glory of the change, civilization and history of the nation.According to him, the government has cut down on the budget for the Society, there is no land plot for the library building and the Social Welfare Council has been increasing the rent of the library block like that for the commercial shops.Society leading persons Mahesh Agrawal and Tirtha Raj Onta were felicitated on the occasion.The Library was registered with the Kathmandu District Administration Office on September 25, 2003. More than 30,000 visitors annually come to the library. The number of annual members is 2,400.The Society started running the library from 2005. The library has received 37,614 books as donation while 4,351 books have been purchased, stated the Society.Balmeeki Vidyapeeth to give land to LibraryOn the occasion, Chief of Balmeeki Vidyapeeth , Prof Dr Bhagawat Dhakal proposed that land plot can be allotted to the Society for constructing a building of the Society for jointly running the Library at the Vidyapeeth's premises.He broached the idea of the Vidyapeeth and the Society together operating the Library as the former was facing shortage of budget for constructing its own library block and suggested the latter to seek funds for constructing the library building.",
+
+    "A baby is a shingle from the right perspective. Before defenses, collars were only operations. Bails are gleesome relatives. An alloy is a streetcar's debt. A fighter of the scarecrow is assumed to be a leisured laundry. A stamp can hardly be considered a peddling payment without also being a crocodile. A skill is a meteorology's fan. Their scent was, in this moment, a hidden feeling. The competitor of a bacon becomes a boxlike cougar.",
+
+    "A broadband jam is a network of the mind. One cannot separate chickens from glowing periods. A production is a faucet from the right perspective. The lines could be said to resemble zincoid females. A deborah is a tractor's whale. Cod are elite japans. Some posit the wiglike norwegian to be less than plashy. A pennoned windchime's burst comes with it the thought that the printed trombone is a supply. Relations are restless tests.",
+
+    "In recent years, some teeming herons are thought of simply as numbers. Nowhere is it disputed that an unlaid fur is a marble of the mind. Far from the truth, few can name a glossy lier that isn't an ingrate bone. The chicken is a giraffe. They were lost without the abscessed leek that composed their fowl. An interviewer is a tussal bomb. Vanward maracas show us how scarfs can be doubts. Few can name an unguled punch that isn't pig.",
+
+    "The government has initiated activities related to iron ore exploration in the country to minimize iron imports from India. Iron extraction efforts have been undertaken following a study conducted by the Department of Mines and Geology, which identified iron ore deposits stretching from west Namjakot to east Dhaubadi under Hupsekot Rural Municipality-5 in Gandaki Province.The decision to pursue activities for the study of iron exploration led to the establishment of 'Dhaubadi Iron Company Limited' in a cabinet meeting held on August 21, 2019. The company was officially registered as 'Dhaubadi Iron Company Ltd' at the Office of the Company Registrar on September 26, 2019, and began operations in February 2020.",
+    "Dr Janak Bahadur Chand, the Chief Executive Officer of the company, stated that the company conducted a comprehensive study on the quality and volume of iron ore deposits, completed geological studies, drilling activities, and chemical testing in the iron mines area. Despite limited resources and personnel, the company has been conducting financial and commercial feasibility studies.The company has initiated the process for land acquisition in Kawasoti Municipality and Devchuli Municipality for the industry.Meanwhile, the government has sent five tonnes of iron to the Sichuan Institute of Metallurgical Geology and Exploration in Chengdu, China, for testing. The Chinese laboratory confirmed that the mine has iron suitable for industrial production.",
+    "Authors often misinterpret the flag as a wayless trigonometry, when in actuality it feels more like a bousy gold. Few can name a jasp oven that isn't a stutter grape. They were lost without the huffy religion that composed their booklet. Those waves are nothing more than pedestrians. Few can name a quartered semicolon that isn't a rounding scooter. Though we assume the latter, the literature would have us believe.",
+
+    "This could be, or perhaps few can name a pasteboard quiver that isn't a brittle alligator. A swordfish is a death's numeric. Authors often misinterpret the mist as a swelling asphalt, when in actuality it feels more like a crosswise closet. Some posit the tonal brother-in-law to be less than newborn. We know that the sizes could be said to resemble sleepwalk cycles. Before seasons, supplies were only fighters. Their stew was, in this moment.",
+    "The government has revoked six Nepali citizenship certificates obtained from the District Administration Office, Siraha, due to the submission of fake details. Minister for Communications and Information Technology, Rekha Sharma, announced the removal of these individuals from Nepali citizenship during a press briefing on the decisions of the Council of Ministers held on December 21 and 28.In other decisions, the cabinet has given in-principle approval to draft a bill related to the formation, operation, and conditions of services of the Nepal Special Service. It also defined indigenous minorities (except Dalits) as those with less than 0.5 percent of the total population of the National Consensus, 2078 BS, for the purpose of the National Assembly Member Election Act, 2075 BS.",
+    "Moreover, the government appointed Ramkrishna Adhiakri and Sushila Chaudhary as advisors to the Gorkhapatra Corporation, approved directives on social media use by government offices, national policies on disabilities and children, and an action plan for implementing the national gender equality policy.The cabinet entrusted the management responsibility of the Annapurna Conservation Area and Manaslu Conservation Area to the National Trust for Nature Conservation.Additionally, the government designated Nepali Ambassador to Malaysia, Dilliraj Poudel, as a non-resident ambassador to the Philippines. It promoted two brigadier generals to the rank of major general and nine colonels to brigadier generals in the Nepal Army.",
+    "The cabinet approved the use of land for the Kathmandu-Tarai-Madhes Expressway Road Project, involving 40.19 hectares in the national forest of Makawanpur and Bara, with the removal of 4,345 trees.It also approved the use of 10.13 hectares of land in the national forest area of Rasuwa for 24 years and the removal of 1,214 trees for the Sanjen Khola Hydropower Project transmission line.Furthermore, Srijana (Malla) Rana and Lokbhakta Shamsher JB Rana were nominated as members of the Dream Garden Development Committee.Public holidays and office timings for 2081 BS were also determined, with extensions granted for Martyrs' Day and Ram Navami, as informed by Minister Sharma.",
+    
+];
+
+const typingText = document.querySelector(".typing-text p")
+const inpField = document.querySelector(".wrapper .input-field")
+const tryAgainBtn = document.querySelector(".content button")
+const timeTag = document.querySelector(".time span b")
+const mistakeTag = document.querySelector(".mistake span")
+const wpmTag = document.querySelector(".wpm span")
+const cpmTag = document.querySelector(".cpm span")
+
+let timer;
+let maxTime = 60;
+let timeLeft = maxTime;
+let charIndex = mistakes = isTyping = 0;
+
+function loadParagraph() {
+    const ranIndex = Math.floor(Math.random() * paragraphs.length);
+    typingText.innerHTML = "";
+    paragraphs[ranIndex].split("").forEach(char => {
+        console.log(char);
+        let span = `<span>${char}</span>`
+        typingText.innerHTML += span;
+    });
+    typingText.querySelectorAll("span")[0].classList.add("active");
+    document.addEventListener("keydown", () => inpField.focus());
+    typingText.addEventListener("click", () => inpField.focus());
+}
+
+function initTyping() {
+    let characters = typingText.querySelectorAll("span");
+    let typedChar = inpField.value.split("")[charIndex];
+    if (charIndex < characters.length - 1 && timeLeft > 0) {
+        if (!isTyping) {
+            timer = setInterval(initTimer, 1000);
+            isTyping = true;
+        }
+        if (typedChar == null) {
+            if (charIndex > 0) {
+                charIndex--;
+                if (characters[charIndex].classList.contains("incorrect")) {
+                    mistakes--;
+                }
+                characters[charIndex].classList.remove("correct", "incorrect");
+            }
+        } else {
+            if (characters[charIndex].innerText == typedChar) {
+                characters[charIndex].classList.add("correct");
+            } else {
+                mistakes++;
+                characters[charIndex].classList.add("incorrect");
+            }
+            charIndex++;
+        }
+        characters.forEach(span => span.classList.remove("active"));
+        characters[charIndex].classList.add("active");
+
+        let wpm = Math.round(((charIndex - mistakes) / 5) / (maxTime - timeLeft) * 60);
+        wpm = wpm < 0 || !wpm || wpm === Infinity ? 0: wpm;
+
+        wpmTag.innerText = wpm;
+        mistakeTag.innerText = mistakes;
+        cpmTag.innerText = charIndex - mistakes;
+    } else {
+        clearInterval(timer);
+        inpField.value = "";
+    }
+}
+
+function initTimer() {
+    if (timeLeft > 0) {
+        timeLeft--;
+        timeTag.innerText = timeLeft;
+        let wpm = Math.round(((charIndex - mistakes) / 5) / (maxTime - timeLeft) * 60);
+        wpmTag.innerText = wpm;
+    } else {
+        clearInterval(timer);
+    }
+}
+
+function resetGame() {
+    loadParagraph();
+    clearInterval(timer);
+    timeLeft = maxTime;
+    charIndex = mistakes = isTyping = 0;
+    inpField.value = "";
+    timeTag.innerText = timeLeft;
+    wpmTag.innerText = 0;
+    mistakeTag.innerText = 0;
+    cpmTag.innerText = 0;
+}
+
+loadParagraph();
+inpField.addEventListener("input", initTyping);
+tryAgainBtn.addEventListener("click", resetGame);
